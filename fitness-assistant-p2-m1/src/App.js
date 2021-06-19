81b462079eb9b12d96efda2588a55708308a004e
akumagamo <charles.lam@gmx.at>
2021-06-19 18:57:30 +0200

    Chapter Three

--- fitness-assistant-p2-m1/src/App.js
+++ fitness-assistant-p2-m1/src/App.js
@@ -9,10 +9,20 @@ import * as tfjsWebGl from '@tensorflow/tfjs-backend-webgl';
 import * as Webcam from 'react-webcam';
 
 
+function getTime(){
+    return (new Date()).getTime();
+}
+
+
 function App() {
+    const POSE_INTERVAL_IN_MS = 100;
     const [model, setModel] = useState({});    
 
+    const [isPoseEstimation, setIsPoseEstimation] = useState(false);    
+    
+
     const webcamRef = useRef(null);
+    const poseEstimationLoop =  useRef(null);
 
     async function loadPosenet(){
 
@@ -29,6 +39,44 @@ function App() {
             console.info('Posenet Model Loaded…');
     }
 
+    const handlePoseEstimation = () =>{
+
+        if(isPoseEstimation){
+            stopPoseEstimation();   
+        } else{
+            startPoseEstimation();
+        }
+        setIsPoseEstimation(!isPoseEstimation);
+    }
+
+    const startPoseEstimation = async () =>{
+        if(webcamRef && webcamRef.current){
+            let currentTime = getTime();
+
+            const video = webcamRef.current.video;
+            const {videoHeight, videoWidth} = webcamRef.current.video;
+
+            webcamRef.current.video.width = videoWidth;
+            webcamRef.current.video.height = videoHeight;
+
+            poseEstimationLoop.current = setInterval( POSE_INTERVAL_IN_MS );
+
+            const pose = await model.estimateSinglePose(video, {
+                flipHorizontal: false
+              });
+
+            const endTime = getTime();
+
+            console.info(pose, endTime,  endTime - currentTime );
+        }   
+
+
+    }
+
+    const stopPoseEstimation = () => {
+        clearInterval(poseEstimationLoop.current)
+    }
+
     useEffect(() =>{
         loadPosenet();
     });
@@ -36,6 +84,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <button onClick={handlePoseEstimation}> {isPoseEstimation ? 'STOP' : 'START'}</button>
       <Webcam
         audio={false}
         width={800}
@@ -44,6 +93,7 @@ function App() {
         screenshotFormat="image/jpeg"
         className="web-cam"
       />
+
       </header>
     </div>
   );
