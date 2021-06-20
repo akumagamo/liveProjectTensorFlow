806c5d28c0c34767be6215081e79e5897571707d
akumagamo <charles.lam@gmx.at>
2021-06-20 12:50:09 +0200

    Chapter Two Part Three

--- fitness-assistant-p3-m1/src/App.js
+++ fitness-assistant-p3-m1/src/App.js
@@ -9,15 +9,45 @@ import * as Webcam from 'react-webcam';
 
 import {drawKeypoints, drawSkeleton} from './utilities';
 
-import { Grid } from  '@material-ui/core';
+import { Grid, makeStyles, AppBar, Toolbar, Typography, Button, Card, CardContent, CardActions } from  '@material-ui/core';
 
 
 
 function getTime(){
     return (new Date()).getTime();
 }
+/*
+function useStyles(){
+    return makeStyles({
+        backgroundAppBar: {
+            background: '#1875d2'
+        },
+        title: {
+            flexGrow: 1,
+            textAlign: 'left'
+        },
+        statsCard:{
+            width: '250px',
+            margin: '10px'
+        }
+      });
+}*/
+const useStyles = makeStyles({
+    backgroundAppBar: {
+        background: '#1875d2'
+    },
+    title: {
+        flexGrow: 1,
+        textAlign: 'left'
+    },
+    statsCard:{
+        width: '250px',
+        margin: '10px'
+    }
+  });
 
 function App() {
+    const classes = useStyles()
     const POSE_INTERVAL_IN_MS = 1000;
 
     const [model, setModel] = useState({});    
@@ -95,8 +125,6 @@ function App() {
                 drawCanvas(pose, videoWidth, videoHeight, canvasRef.current);                
             }, POSE_INTERVAL_IN_MS );
         }   
-
-
     }
 
     const stopPoseEstimation = () => {
@@ -110,21 +138,97 @@ function App() {
   return (
     <div className="App">
         <Grid spacing={3} container={true} >
+            <AppBar position='static' className={classes.backgroundAppBar} >
+                <Toolbar variant='dense'>
+                    <Typography
+                        variant='h6'
+                        color='inherit'
+                        className={classes.title}
+                    >Fitness Assistant</Typography>
+                </Toolbar>
+                <Button color='inherit'>Start Workout</Button>
+                <Button color='inherit'>History</Button>
+                <Button color='inherit'>Reset</Button>
+            </AppBar>
             <Grid spacing={3} container={true} >
-                <Grid spacing={3} container={true} item={true} xs={12} >
-
-                </Grid>
+                <Card>
+                    <CardContent>
+                        <Webcam
+                            audio={false}
+                            width={400}
+                            height={300}
+                            ref={webcamRef}
+                            screenshotFormat="image/jpeg"
+     
+                            style={{
+                                marginLeft: "auto",
+                                marginRight: "auto",
+                                marginTop: '10px',
+                                marginBottom: '10px',
+                        
+                                textAlign: "center",
+                                zindex: 9,
+                                width: 800,
+                                height: 600,
+                              }}
+                        />
+                        <canvas 
+                         style={{
+                            position: "absolute",
+                            marginLeft: "auto",
+                            marginRight: "auto",
+                            marginTop: '10px',
+                            marginBottom: '10px',
+                            textAlign: "center",
+                            zindex: 9,
+                            width: 800,
+                            height: 600,
+                            left:0,
+                          }}
+                            ref={canvasRef}>    
+                        </canvas>
+                    </CardContent>
+                    <CardActions style={{justifyContent:'center'}}>
+                        <Grid spacing={0} container={true}>
+                            <Grid item={true} xs={12}>
+                                <Toolbar style={{justifyContent:'center'}}>
+                                    <Card className={classes.statsCard}>
+                                        <CardContent>
+                                            <Typography 
+                                                className={classes.title}
+                                                color='textSecondary'
+                                                gutterBottom={true}
+                                                >Jumping Jacks</Typography>
+                                            <Typography variant='h2' component='h2' color='secondary'>75</Typography>
+                                        </CardContent>
+                                    </Card>
+                                    <Card className={classes.statsCard}>
+                                        <CardContent>
+                                            <Typography 
+                                                className={classes.title}
+                                                color='textSecondary'
+                                                gutterBottom={true}
+                                                >Wall-sit</Typography>
+                                            <Typography variant='h2' component='h2' color='secondary'>200</Typography>
+                                        </CardContent>
+                                    </Card>
+                                    <Card className={classes.statsCard}>
+                                        <CardContent>
+                                            <Typography 
+                                                className={classes.title}
+                                                color='textSecondary'
+                                                gutterBottom={true}
+                                                >Lunges</Typography>
+                                            <Typography variant='h2' component='h2' color='secondary'>5</Typography>
+                                        </CardContent>
+                                    </Card>
+                                </Toolbar>
+                            </Grid>
+                        </Grid>
+                    </CardActions>
+                </Card>
             </Grid>
         </Grid>
-        {/*} <Webcam
-        audio={false}
-        width={400}
-        height={300}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        className="web-cam"
-      />
-        <canvas className='my-canvas' ref={canvasRef}></canvas> */}
     </div>
   );
 }
