776886917835dcc4aea3290fd255140244c86d50
akumagamo <charles.lam@gmx.at>
2021-06-20 08:56:48 +0200

    Chapter Four Part One

--- fitness-assistant-p1-m2/src/App.js
+++ fitness-assistant-p1-m2/src/App.js
@@ -2,12 +2,51 @@ import logo from './logo.svg';
 import * as tf from '@tensorflow/tfjs';
 import './App.css';
 
+
+const doTraining = async (model, xs, ys) => {
+    const history =
+      await model.fit(xs, ys,
+        {
+          epochs: 200,
+          callbacks: {
+            onEpochEnd: async (epoch, logs) => {
+              console.info("Epoch:" + epoch,  logs);
+            }
+          }
+        });
+    console.log(history.params);
+  }
+  
 function App() {
 
 
 const handleRunTraining = () =>{
     console.info('Run Training');
-    console.info(tf.tensor([1, 2, 3, 4]));
+    
+    const model = tf.sequential();
+    model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+  
+    model.compile({ optimizer: 
+            tf.train.sgd(0.01), 
+            loss: 'meanSquaredError' });
+    model.summary();
+
+     // Equation: y = 2x - 1
+    const equation = (x) => 2* x -1; 
+    const xValues = [ -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const yValues = xValues.map( equation);
+    const xs = tf.tensor2d(xValues, [xValues.length, 1]);
+    const ys = tf.tensor2d(yValues, [yValues.length, 1]);
+    
+    doTraining(model, xs, ys).then(() => {
+        const prediction = model.predict(tf.tensor2d([10], [1, 1]));
+        var result = prediction.dataSync();
+        prediction.dispose();
+        
+        console.info(result)
+        console.info('Result: ' + result[0]);
+    });
+
 };
 
 
